05775a07dd79b56d95d2b10840501ea02304a80c
Travis You <1244098000@qq.com>
2022-06-24 10:01:09 +0800

    feat(Tiger): add a class-'Tiger' extends 'Felidae'

---
+++ src/com/travis/Tiger.java
@@ -0,0 +1,7 @@
+package com.travis;
+
+public class Tiger extends Felidae{
+    public Tiger(int age, double weight, boolean eating, boolean running) {
+        super(age, weight, eating, running);
+    }
+}
